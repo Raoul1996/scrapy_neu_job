2f91383ab5f93f383eca00bd85367a77c3a1a549
Raoul1996 <baobaozhou199606@gmail.com>
2018-10-05 23:38:35 +0800

    add next page info
    Change-Id: Iba0a5f294b3258f96f99617b724840d23053627d

---
+++ tutorial/spiders/list_spider.py
@@ -0,0 +1,26 @@
+# -*-coding:utf-8-*-
+import scrapy
+import logging
+
+
+class ListSpider(scrapy.Spider):
+    name = 'list'
+    start_urls = ['http://lab.scrapyd.cn']
+
+    def parse(self, response):
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            text = quote.css('.text::text').extract_first()
+            author = quote.css('.author::text').extract_first()
+            tags = quote.css('.tags .tag::text').extract()
+            tags = ','.join(tags)
+            filename = '%s-quotes.txt' % author
+            with open(filename, 'a+') as f:
+                f.write(text.encode('utf-8'))
+                f.write('\n')
+                f.write('tag: ' + tags.encode('utf-8'))
+                f.write('\n\n')
+                f.close()
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield scrapy.Request(next_page, callback=self.parse)
